33a720f410a1168b5074bfa00dbba685cdae1965
JuliaCherepnev <yuliya.cherepnev@mail.ru>
2024-03-30 14:25:11 +0400

    Messege changed to "Привет Мир!"

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@
 public class Main {
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello World!");
+        System.out.print("Привет Мир!");
     }
 }
